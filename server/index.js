145134bea236801743f9959d2745db707ba2c256
hugolpa <contatohugoa@gmail.com>
2022-06-28 13:32:51 -0300

    cros lines update on server

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const db = mysql.createPool({
   database: "heroku_f30eb8446fe52c2",
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+app.use(cors()); // <---- use cors middleware
 // mysql://b597b0eda9d9c5:2c734e5f@us-cdbr-east-05.cleardb.net/heroku_f30eb8446fe52c2?reconnect=true
 
 
